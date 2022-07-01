1cfd34ec1a75691b7499df6fe14d190457d83332
hayeonkimm <hayeon.data@gmail.com>
2022-07-02 01:36:56 +0900

    업데이트 - kakao.js
    변경사항 :

--- server/routes/kakao.js
+++ server/routes/kakao.js
@@ -5,17 +5,18 @@ var router = express.Router();
 const KAKAO_OAUTH_TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
 const grant_type = 'authorization_code';
 const client_id = process.env.client_id;
-console.log(client_id);
-const redirect_uri = 'http://52.78.168.151:3000//oauth/callback/kakao';
+const redirect_uri = 'http://localhost:3000/oauth/callback/kakao';
+const axios = require('axios');
+
+let access_token = '';
 
 router.get('/oauth/callback/kakao', function (req, res, next) {
   let code = req.query.code;
-
   console.log('code: ', code);
-  console.log('redirect_uri: ', redirect_uri);
-  console.log('client_id: ', client_id);
-  console.log('grant_type: ', grant_type);
-  console.log(KAKAO_OAUTH_TOKEN_API_URL);
+  // console.log('redirect_uri: ', redirect_uri);
+  // console.log('client_id: ', client_id);
+  // console.log('grant_type: ', grant_type);
+  // console.log(KAKAO_OAUTH_TOKEN_API_URL);
 
   try {
     axios
@@ -30,10 +31,12 @@ router.get('/oauth/callback/kakao', function (req, res, next) {
         }
       )
       .then((result) => {
-        console.log('result:', result);
-        console.log('엑세스 토큰:', result.data['access_token']);
-        // 토큰을 활용한 로직을 적어주면된다.
+        // console.log('result:', result);
+        // console.log('엑세스 토큰:', result.data['access_token']);
+        access_token = result.data['access_token'];
+        res.send(result['access_token']);
 
+        // 토큰을 활용한 로직을 적어주면된다.
         // url, 화면전환 코드 작성!
       })
       .catch((e) => {
@@ -45,24 +48,33 @@ router.get('/oauth/callback/kakao', function (req, res, next) {
     res.send(e);
   }
 });
-
-router.post('/kakao/information', function (req, res, next) {
-  const data = req.body;
-  console.log(data);
-  res.send(data);
+console.log(access_token);
+router.get('/kakao/information', function (req, res, next) {
+  console.log(access_token);
+  res.send(access_token);
 });
 
 router.post('/google/information', function (req, res, next) {
-  const data = req.body;
-  console.log(data);
-  res.send(data);
+  res.send('hi');
 });
 
 router.post('/naver/information', function (req, res, next) {
   const data = req.body;
-  console.log(data);
+  console.log('data:', data);
   res.send(data);
 });
+
+// router.get('/oauth/:coperation', async (req, res) => {
+//   const coperation = req.params.coperation;
+//   const code = req.param('code');
+//   const options = getOption(coperation, code);
+//   const token = await getAccessToken(options);
+//   const userInfo = await getUserInfo(options.userInfoUrl, token.access_token);
+
+//   // TODO Redirect Frot Server (쿠키, 세션, local_store 중에 로그인을 유지한다.)
+//   // TODO Data Base or 쿠키 reflesh Token 저장 방법 모색
+//   res.send(userInfo);
+// });
 
 // 리다이렉트될 화면
 
