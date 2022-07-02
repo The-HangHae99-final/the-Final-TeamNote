d98fb443e212d0a24f38d1bd875011baba37d8e8
hayeonkimm <hayeon.data@gmail.com>
2022-07-03 01:05:38 +0900

    업데이트

--- server/app.js
+++ server/app.js
@@ -67,6 +67,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
+const corsOptions = {
+  origin: '*',
+};
+app.use(cors(corsOptions));
+
 //실시간 채팅
 const io = new Server(server, {
   cors: {
