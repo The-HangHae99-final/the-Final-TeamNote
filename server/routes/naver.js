c13a7b09b64ee8fd7388be4c899ef7e4cc4cc02a
hayeonkimm <hayeon.data@gmail.com>
2022-07-03 01:37:03 +0900

    업데이트

--- server/routes/naver.js
+++ server/routes/naver.js
@@ -1,9 +1,7 @@
 const express = require('express');
 const router = express.Router();
 
-router.get('/account/sign-in', function (req, res, next) {
-  
- 
+router.get('/auth/login/callback', function (req, res, next) {
   const data = req.body;
   console.log('data:', data);
   res.send(data);
