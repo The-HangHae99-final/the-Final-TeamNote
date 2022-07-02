01ae6a77631392d6b33644efbfbaeccc2d42a691
hayeonkimm <hayeon.data@gmail.com>
2022-07-02 19:19:42 +0900

    수정 - naver 작업 이전으로 복구

--- server/schemas/access_token.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Acess_tokenSchema = new mongoose.Schema({
-  access_token: {
-    type: String,
-  },
-});
-
-module.exports = mongoose.model('Acesstoken', Acess_tokenSchema);
