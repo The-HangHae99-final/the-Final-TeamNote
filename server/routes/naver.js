a28f45db9d59e511316f9152f715573213f5312d
hayeonkimm <hayeon.data@gmail.com>
2022-07-03 14:53:45 +0900

    수정후 재배포

--- server/routes/naver.js
+++ server/routes/naver.js
@@ -41,7 +41,7 @@ router.post('/naver', function (req, res) {
     if (!error && response.statusCode == 200) {
       res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
       res.end(body);
-      console.log(res);
+      console.log('res:', res);
     } else {
       res.status(response.statusCode).end();
       console.log('error = ' + response.statusCode);
