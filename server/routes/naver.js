7b5441b58a21676551b75a0b554e9eda1f666e87
hayeonkimm <hayeon.data@gmail.com>
2022-07-03 13:43:44 +0900

    수정후 재배포

--- server/routes/naver.js
+++ server/routes/naver.js
@@ -22,52 +22,35 @@ var client_secret = process.env.YOUR_CLIENT_SECRET;
 var state = 'teamnote';
 var redirectURI = encodeURI('');
 var api_url = 'http://52.78.168.151:3000/auth/login/callback';
-app.get('/naverlogin', function (req, res) {
+
+router.post('/naver', function (req, res) {
+  var code = req.body.code;
+  var state = req.body.state;
   api_url =
-    'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' +
+    'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
     client_id +
-    '&redirect_uri=' +
-    redirectURI +
+    '&client_secret=' +
+    client_secret +
+    '&code=' +
+    code +
     '&state=' +
     state;
-  res.writeHead(200, { 'Content-Type': 'text/html;charset=utf-8' });
-  res.end(
-    "<a href='" +
-      api_url +
-      "'><img height='50' src='http://static.nid.naver.com/oauth/small_g_in.PNG'/></a>"
-  );
-  // 여기?
-  app.get('/callback', function (req, res) {
-    code = req.query.code;
-    state = req.query.state;
-    api_url =
-      'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id=' +
-      client_id +
-      '&client_secret=' +
-      client_secret +
-      '&redirect_uri=' +
-      redirectURI +
-      '&code=' +
-      code +
-      '&state=' +
-      state;
-    var request = require('request');
-    var options = {
-      url: api_url,
-      headers: {
-        'X-Naver-Client-Id': client_id,
-        'X-Naver-Client-Secret': client_secret,
-      },
-    };
-    request.get(options, function (error, response, body) {
-      if (!error && response.statusCode == 200) {
-        res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
-        res.end(body);
-      } else {
-        res.status(response.statusCode).end();
-        console.log('error = ' + response.statusCode);
-      }
-    });
+  var request = require('request');
+  var options = {
+    url: api_url,
+    headers: {
+      'X-Naver-Client-Id': client_id,
+      'X-Naver-Client-Secret': client_secret,
+    },
+  };
+  request.get(options, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
+      res.end(body);
+    } else {
+      res.status(response.statusCode).end();
+      console.log('error = ' + response.statusCode);
+    }
   });
 });
 
