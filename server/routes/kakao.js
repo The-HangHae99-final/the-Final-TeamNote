eccd090307c101a43c740e24078ad7637fe9feaf
hayeonkimm <hayeon.data@gmail.com>
2022-07-04 13:43:35 +0900

    업데이트 - kakao.js 변경사항: kakao.js 코드추가

--- server/routes/kakao.js
+++ server/routes/kakao.js
@@ -2,15 +2,45 @@ const dotenv = require('dotenv').config();
 // const Acess_ = require('../schemas/access_token');
 var express = require('express');
 var router = express.Router();
-const KAKAO_OAUTH_TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
-const grant_type = 'authorization_code';
-const client_id = process.env.kakao_client_id;
-const redirect_uri = 'http://localhost:3000/auth/login/kakao/callback';
 const axios = require('axios');
 const { request } = require('express');
 
 // Rediect URI : http://localhost:3000/auth/login/kakao/callback
 //로직
+
+var express = require('express');
+var router = express.Router();
+const KAKAO_OAUTH_TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
+const KAKAO_GRANT_TYPE = 'authorization_code';
+const KAKAO_CLIENT_id = process.env.kakao_client_id;
+const KAKAO_REDIRECT_URL = 'http://localhost:3000/auth/login/kakao/callback';
+
+router.get('/oauth/callback/kakao', function (req, res, next) {
+  let code = req.query.code;
+  try {
+    axios
+      .post(
+        `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`,
+        {
+          headers: {
+            'Content-type': 'application/x-www-form-urlencoded;charset=utf-8',
+          },
+        }
+      )
+      .then((result) => {
+        console.log(result.data['access_token']);
+        res.send(result.data['access_token']);
+        // 토큰을 활용한 로직을 적어주면된다.
+      })
+      .catch((e) => {
+        console.log(e);
+        res.send(e);
+      });
+  } catch (e) {
+    console.log(e);
+    res.send(e);
+  }
+});
 
 // 프론트에게서 인가코드를 받는다 post_1
 // 서버에서 인가코드를 가지고 카톡에게서 토큰을 받는다.
@@ -24,38 +54,38 @@ const { request } = require('express');
 // /naver =>/oauth/callback/kakao 인가코드 넘기는 url
 // kakao/member token 넘기는 url
 
-let access_token = '';
+// let access_token = '';
 
-router.post('/oauth/callback/kakao', async function (req, res, next) {
-  let code = req;
-  console.log('code: ', code);
-  // console.log('redirect_uri: ', redirect_uri);
-  // console.log('client_id: ', client_id);
-  // console.log('grant_type: ', grant_type);
-  // console.log(KAKAO_OAUTH_TOKEN_API_URL);
-  // var header = 'Bearer ' + token;
-  var api_url = `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`;
-  var options = {
-    url: api_url,
+// router.post('/oauth/callback/kakao', async function (req, res, next) {
+//   let code = req.body.code;
+//   console.log('code: ', code);
+//   // console.log('redirect_uri: ', redirect_uri);
+//   // console.log('client_id: ', client_id);
+//   // console.log('grant_type: ', grant_type);
+//   // console.log(KAKAO_OAUTH_TOKEN_API_URL);
+//   // var header = 'Bearer ' + token;
+//   var api_url = `${KAKAO_OAUTH_TOKEN_API_URL}?grant_type=${grant_type}&client_id=${client_id}&redirect_uri=${redirect_uri}&code=${code}`;
+//   var options = {
+//     url: api_url,
 
-    headers: {
-      'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
-      'Content-Type': 'application / json',
-    },
-  };
-  request.get(options, function (error, response, body) {
-    if (!error && response.statusCode == 200) {
-      res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
-      res.end(body);
-    } else {
-      console.log('error');
-      if (response != null) {
-        res.status(response.statusCode).end();
-        console.log('error = ' + response.statusCode);
-      }
-    }
-  });
-});
+//     headers: {
+//       'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8',
+//       'Content-Type': 'application / json',
+//     },
+//   };
+//   request.get(options, function (error, response, body) {
+//     if (!error && response.statusCode == 200) {
+//       res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
+//       res.end(body);
+//     } else {
+//       console.log('error');
+//       if (response != null) {
+//         res.status(response.statusCode).end();
+//         console.log('error = ' + response.statusCode);
+//       }
+//     }
+//   });
+// });
 router.post('/kakao/member', function (req, res) {
   var api_url = 'https://kapi.kakao.com/v2/user/me';
   var request = require('request');
