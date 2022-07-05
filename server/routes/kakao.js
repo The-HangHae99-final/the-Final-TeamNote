a0a9f77144e2d701caa50d955a2d256c6186db26
hayeonkimm <hayeon.data@gmail.com>
2022-07-05 10:58:21 +0900

    저장 - all files 변경사항:

--- server/routes/kakao.js
+++ server/routes/kakao.js
@@ -6,8 +6,16 @@ const axios = require('axios');
 const { request } = require('express');
 var socialUser = require('../schemas/social_user');
 
-// Rediect URI : http://localhost:3000/auth/login/kakao/callback
 //로직
+
+// 프론트에게서 인가코드를 받는다 post_1
+// 서버에서 인가코드를 가지고 카톡에게서 토큰을 받는다.
+// 토큰을 클라이언트에게 보낸다.
+// 클라이언트가 토큰을 바디에 담아서 다시 post 요청을 한다. post_2
+// 백엔드에서 토큰을 가지고 다시 카톡에게 정보를 요청한다.
+// 정보를 클라이언트에게 보낸다.
+// 클라이언트가 받고 데이터를 파싱해서 다시 보낸다.post_3  // 데이터 파싱문제!!
+// 백엔드가 받아서 DB에 저장한다.
 
 var express = require('express');
 var router = express.Router();
@@ -43,15 +51,6 @@ router.post('/auth/login/kakao/callback', function (req, res, next) {
     res.send(e);
   }
 });
-
-// 프론트에게서 인가코드를 받는다 post_1
-// 서버에서 인가코드를 가지고 카톡에게서 토큰을 받는다.
-// 토큰을 클라이언트에게 보낸다.
-// 클라이언트가 토큰을 바디에 담아서 다시 post 요청을 한다. post_2
-// 백엔드에서 토큰을 가지고 다시 카톡에게 정보를 요청한다.
-// 정보를 클라이언트에게 보낸다.
-// 클라이언트가 받고 데이터를 파싱해서 다시 보낸다.post_3  // 데이터 파싱문제!!
-// 백엔드가 받아서 DB에 저장한다.
 
 // /naver =>/oauth/callback/kakao 인가코드 넘기는 url
 // kakao/member token 넘기는 url
