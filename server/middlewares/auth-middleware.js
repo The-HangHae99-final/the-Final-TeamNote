4f0d9551a46f0d0ef5f1147a08bf54ca31587f0d
hayeonkimm <hayeon.data@gmail.com>
2022-07-05 23:28:50 +0900

    업데이트 - auth-middleware.js 수정사항 :

--- server/middlewares/auth-middleware.js
+++ server/middlewares/auth-middleware.js
@@ -5,36 +5,46 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (authorization == null) {
+    console.log('authorization: ', authorization);
     res.status(401).send({
-      errorMessage: '로그인이 필요합니다.-------null------------',
+      errorMessage: '로그인이 필요합니다.----------null------------',
     });
     return;
   }
 
   const [tokenType, tokenValue] = authorization.split(' ');
+  console.log('tokenValue: ', tokenValue);
+  console.log('tokenType: ', tokenType);
+  
 
   if (tokenType !== 'Bearer') {
+    console.log('tokenType: ', tokenType);
     res.status(401).send({
-      errorMessage: '로그인이 필요합니다.---------Bearer--------',
+      errorMessage: '로그인이 필요합니다.---------Bearer----------',
     });
     return;
   }
 
   try {
     const myToken = verifyToken(tokenValue);
+    console.log('myToken: ', myToken);
     if (myToken == 'jwt expired') {
       // access token 만료
       const userInfo = jwt.decode(tokenValue, 'secret');
-      console.log(userInfo);
+      console.log('userInfo: ', userInfo);
       const userId = userInfo.userId;
       let refresh_token;
       user.findOne({ where: userId }).then((u) => {
         refresh_token = u.refresh_token;
         const myRefreshToken = verifyToken(refresh_token);
         if (myRefreshToken == 'jwt expired') {
-          res.send({ errorMessage: '로그인이 필요합니다.------expired------' });
+          console.log('myRefreshToken: ', myRefreshToken);
+          res.send({
+            errorMessage: '로그인이 필요합니다.---------expired----------',
+          });
         } else {
           const myNewToken = jwt.sign({ userId: u.userId }, 'secret', {
+            console.log('myNewToken: ', myNewToken);
             expiresIn: '1200s',
           });
           res.send({ message: 'new token', myNewToken });
@@ -42,6 +52,7 @@ module.exports = (req, res, next) => {
       });
     } else {
       const { userId } = jwt.verify(tokenValue, 'secret');
+      console.log('userId: ', userId);
       user.findOne({ where: userId }).then((u) => {
         res.locals.user = u;
         next();
