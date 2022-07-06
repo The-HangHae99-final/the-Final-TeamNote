118a00e0f0ba9ecc0ab87ce9d1a257253fc7ffe6
hayeonkimm <hayeon.data@gmail.com>
2022-07-06 18:51:38 +0900

    업데이트 - 수리중 수정사항 :

--- server/controller/kakaos.js
+++ server/controller/kakaos.js
@@ -11,7 +11,7 @@ var express = require('express');
 var router = express.Router();
 const KAKAO_OAUTH_TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
 const KAKAO_GRANT_TYPE = 'authorization_code';
-const client_id = process.env.kakao_client_id;
+const client_id = process.env.client_id;
 const KAKAO_REDIRECT_URL = 'http://localhost:3000/auth/login/kakao/callback';
 // router.post(
 //   '/auth/login/kakao/callback',
