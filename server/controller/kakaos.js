912810e0d9120c4bedf870238c455388b9888d49
hayeonkimm <hayeon.data@gmail.com>
2022-07-07 22:05:16 +0900

    업데이트 - kakaos.js 복귀
    변경사항 :

--- server/controller/kakaos.js
+++ server/controller/kakaos.js
@@ -1,36 +1,26 @@
 const dotenv = require('dotenv').config();
+// const Acess_ = require('../schemas/access_token');
 var express = require('express');
 var router = express.Router();
 const axios = require('axios');
 const { request } = require('express');
 const jwt = require('jsonwebtoken');
 var socialUser = require('../schemas/social_user');
+// Rediect URI : http://localhost:3000/auth/login/kakao/callback
+//로직
 var express = require('express');
 var router = express.Router();
 const KAKAO_OAUTH_TOKEN_API_URL = 'https://kauth.kakao.com/oauth/token';
 const KAKAO_GRANT_TYPE = 'authorization_code';
 const client_id = process.env.client_id;
-// console.log('client_id---------------: ', client_id);
+console.log('client_id---------------: ', client_id);
 const KAKAO_REDIRECT_URL = 'http://localhost:3000/auth/login/kakao/callback';
-
-// 전체적 로직 (설명: hayeonkimm)
-
-// 프론트에게서 인가코드를 받는다 - auth/login/kakao/callback
-// 서버에서 인가코드를 가지고 카톡에게서 토큰을 받는다.
-// 토큰을 클라이언트에게 보낸다.
-// 클라이언트가 토큰을 바디에 담아서 다시 post 요청을 한다.
-// 백엔드에서 토큰을 가지고 다시 카톡에게 정보를 요청한다. - /kakao/member
-// 정보를 클라이언트에게 보낸다.
-// 클라이언트가 받고 데이터를 파싱해서 다시 보낸다.  // 데이터 파싱문제
-// 백엔드가 받아서 DB에 저장한다. - /kakao/parsing
-
-// post - '/auth/login/kakao/callback'
-// 인가코드를 받고, url_api로 액세스 토큰과 데이터를 요청한다.
-
+// router.post(
+//   '/auth/login/kakao/callback',
 function kakao_callback(req, res, next) {
   try {
-    let code = req.body.code; // 프론트로부터 인가코드 받기
-    console.log('인가코드:' + code);
+    let code = req.body.code;
+    console.log('코드다..........' + code);
     try {
       axios
         .post(
@@ -42,27 +32,35 @@ function kakao_callback(req, res, next) {
           }
         )
         .then((result) => {
-          console.log('엑세스토큰 :' + result.data['access_token']);
-          res.send(result.data['access_token']); // 토큰을 클라이언트에게 보낸다.
+          console.log('korea' + result);
+          console.log('엑세스토큰------' + result.data['access_token']);
+          res.send(result.data['access_token']);
+          // 토큰을 활용한 로직을 적어주면된다.
         })
-        .catch((error) => {
-          res.send(error);
-          console.log(error);
+        .catch((e) => {
+          console.log(e);
+          res.send(e);
         });
-    } catch (error) {
-      res.send(error);
-      console.log(error);
+    } catch (e) {
+      console.log(e);
+      res.send(e);
     }
-  } catch (error) {
+  } catch (err) {
     res.status(400).send('에러가 발생했습니다.');
     // console.log('error =' + err);
-    console.log(error);
+    console.log(message.err);
   }
 }
+// 프론트에게서 인가코드를 받는다 post_1
+// 서버에서 인가코드를 가지고 카톡에게서 토큰을 받는다.
+// 토큰을 클라이언트에게 보낸다.
+// 클라이언트가 토큰을 바디에 담아서 다시 post 요청을 한다. post_2
+// 백엔드에서 토큰을 가지고 다시 카톡에게 정보를 요청한다.
+// 정보를 클라이언트에게 보낸다.
+// 클라이언트가 받고 데이터를 파싱해서 다시 보낸다.post_3  // 데이터 파싱문제!!
+// 백엔드가 받아서 DB에 저장한다.
 
-// post - '/kakao/member'
-// 엑세스토큰을 받고, url_api로 정보를 요청한다.
-
+// router.post('/kakao/member',
 function kakao_member(req, res) {
   try {
     var api_url = 'https://kapi.kakao.com/v2/user/me';
@@ -77,7 +75,7 @@ function kakao_member(req, res) {
     };
     request.get(options, function (error, response, body) {
       if (!error && response.statusCode == 200) {
-        res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' }); // text/json;charset=utf-8 형태로 보낸다.
+        res.writeHead(200, { 'Content-Type': 'text/json;charset=utf-8' });
         res.end(body);
         console.log(body);
       } else {
@@ -93,20 +91,17 @@ function kakao_member(req, res) {
     console.log('error =' + err);
   }
 }
-
-// post - '/kakao/parsing'
-// 프론트측에서 파싱한 데이터를 넘겨받아서 디비에 저장한다.
-
+// router.post('/kakao/parsing',
 async function kakao_parsing(req, res) {
   try {
     const site = 1; //kakao
     const user_info = req.body;
     console.log('user_info = ' + user_info);
-    const _user = user_info.user_id;
+    const userId = user_info.user_id;
     console.log('userid: ', userId);
     const email = user_info.user_email;
     console.log('email: ', email);
-    const userName = user_info.user_name;
+    const nickname = user_info.user_name;
     console.log('nickname: ', nickname);
     const double = await socialUser.findOne({ email });
     console.log('double: ', double);
@@ -132,10 +127,10 @@ async function kakao_parsing(req, res) {
     } else {
       // 다른 경우라면,
       // 기존에서 리프레시 토큰만 대체하기
-      // 예외조건넣기. 유저가 디비에 있으면 저장하지않기.
       await double.update({ refresh_token }, { where: { email } });
       res.send(token);
     }
+    // 예외조건넣기. 유저가 디비에 있으면 저장하지않기.
   } catch (err) {
     res.status(400).send('에러가 발생했습니다.');
     console.log('error =' + err);
