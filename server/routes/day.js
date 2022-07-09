aa6d736c272f7c4b2476ba5d8cec5644884f9bb5
hayeonkimm <hayeon.data@gmail.com>
2022-07-09 16:10:36 +0900

    업데이트 - swagger 관련 파일 업로드

--- server/routes/day.js
+++ server/routes/day.js
@@ -4,9 +4,9 @@ const router = express.Router();
 // 날짜 API
 
 router.get('/day', (req, res) => {
-  //#swagger.tags= ['swagger'];
-  //# swagger.summary= '북마크'
-  //# swagger.description='북마크추가
+  //swagger.tags= ['swagger'];
+  // swagger.summary= '북마크'
+  //swagger.description='북마크추가
 
   const dayBox = [
     '2022-06-02',
