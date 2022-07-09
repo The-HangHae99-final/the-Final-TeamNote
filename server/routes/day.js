36119db8917b00effaf1867f0b0ec88f8809643a
hayeonkimm <hayeon.data@gmail.com>
2022-07-09 16:01:22 +0900

    업데이트 - routes.js

--- server/routes/day.js
+++ server/routes/day.js
@@ -4,6 +4,10 @@ const router = express.Router();
 // 날짜 API
 
 router.get('/day', (req, res) => {
+  //#swagger.tags= ['swagger'];
+  //# swagger.summary= '북마크'
+  //# swagger.description='북마크추가
+
   const dayBox = [
     '2022-06-02',
     '2022-06-02',
