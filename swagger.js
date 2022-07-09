74b6d0248042ddc15087c02dd8840fd5f1f73c7a
hayeonkimm <hayeon.data@gmail.com>
2022-07-09 15:51:39 +0900

    업데이트 - swagger.js
    변경사항 :

--- swagger.js
+++ swagger.js
@@ -4,5 +4,21 @@ const swaggerAutogen = require('swagger-autogen')();
 const outputFile = './swagger_output.json'; // swagger-autogen이 실행 후 생성될 파일 위치와 이름
 const endpointsFiles = ['./server/routes/*']; // 읽어올 Router가 정의되어 있는 js파일들
 
+const options = {
+  swaggerDefinition: {
+    openapi: '3.0.0',
+    info: {
+      title: 'Express Service with Swagger!',
+      version: '1.0.0',
+      description: 'A REST API using swagger and express.',
+    },
+    servers: [
+      {
+        url: 'http://localhost:4040',
+      },
+    ],
+  },
+  apis: [],
+};
 //아래 코드에 .then() 이후를 삭제한다.
-swaggerAutogen(outputFile, endpointsFiles);
+swaggerAutogen(outputFile, endpointsFiles, options);
