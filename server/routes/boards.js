603f8c2a11f083d8b702d6edb7152ea8175943e2
hayeonkimm <hayeon.data@gmail.com>
2022-07-10 20:26:37 +0900

    업데이트 - boards.js
    변경사항 :

--- server/routes/boards.js
+++ server/routes/boards.js
@@ -1,24 +1,24 @@
 const express = require('express');
 const router = express.Router();
-const postController = require('../controller/posts');
+const boardController = require('../controller/boards');
 const authMiddleware = require('../middlewares/auth-middleware');
 
-router.post('/board/notice', authMiddleware, postController.postUpload);
+router.post('/board/notice', authMiddleware, postController.boardUpload);
 
 // 글 전체 조회(임시)
-router.get('/board/notice', postController.postAllView);
+router.get('/board/notice', boardController.boardAllView);
 
 // 글 한개 조회
-router.get('/board/notice/:postId', postController.postView);
+router.get('/board/notice/:postId', boardController.boardView);
 
 // 글 수정
-router.put('/board/notice/:postId', authMiddleware, postController.postEdit);
+router.put('/board/notice/:postId', authMiddleware, boardController.boardEdit);
 
 // 글 삭제
 router.delete(
   '/board/notice/:postId',
   authMiddleware,
-  postController.postDelete
+  boardController.boardDelete
 );
 
 module.exports = router;
