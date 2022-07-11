57cab5cfb4eb24fcf6f1aaef571fcd255728f718
hayeonkimm <hayeon.data@gmail.com>
2022-07-11 23:35:07 +0900

    test files 업로드

--- test/db.test.js
+++ test/db.test.js
@@ -1,31 +1,36 @@
-const dotenv = require('dotenv').config();
-const mongoose = require('mongoose');
+// const dotenv = require('dotenv').config();
+// const mongoose = require('mongoose');
 
-const { MongoClient } = require('mongodb');
+// const { MongoClient } = require('mongodb');
 
-describe('insert', () => {
-  let connection;
-  let db;
+// describe('insert', () => {
+//   let connection;
+//   let db;
 
-  beforeAll(async () => {
-    connection = await MongoClient.connect(process.env.DB_NAME, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-    db = await connection.db('teamnote');
-  });
+//   beforeAll(async () => {
+//     connection = await MongoClient.connect(process.env.DB_NAME, {
+//       useNewUrlParser: true,
+//       useUnifiedTopology: true,
+//     });
+//     db = await connection.db('teamnote');
+//   });
 
-  afterAll(async () => {
-    await connection.close();
-  });
+//   afterAll(async () => {
+//     await connection.close();
+//   });
 
-  it('should insert a doc into collection', async () => {
-    const users = db.collection('users');
+//   it('should insert a doc into collection', async () => {
+//     const users = db.collection('users');
 
-    const mockUser = { _id: 'some-user-id', name: 'John' };
-    await users.insertOne(mockUser);
+//     const mockUser = {
+//       userName: 'some-user-id11',
+//       userEmail: 'John@naver.com',
+//       password: '1234',
+//     };
+//     await users.insertOne(mockUser);
 
-    const insertedUser = await users.findOne({ _id: 'some-user-id' });
-    expect(insertedUser).toEqual(mockUser);
-  });
-});
+//     const insertedUser = await users.findOne({ userName: 'some-user-id11' });
+//     console.log(insertedUser);
+//     expect(insertedUser.userName).toEqual(mockUser.userName);
+//   });
+// });
