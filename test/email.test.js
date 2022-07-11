5136d019d3665e1d90903084a48a5794bae4e9f8
hayeonkimm <hayeon.data@gmail.com>
2022-07-11 22:21:37 +0900

    업데이트 - delete files
    변경사항 :

--- test/email.test.js
+++
@@ -1,9 +0,0 @@
-const validator = require('email-validator');
-
-test('테스트가 성공하는 상황', () => {
-  expect(validator.validate('이메일이 아니에요')).toEqual(false);
-});
-
-test('테스트가 실패하는 상황', () => {
-  expect(validator.validate('my-email@domain.com')).toEqual(true);
-});
