f242a72aa9e38ab978e81ce6b852ca86d60932c6
hayeonkimm <hayeon.data@gmail.com>
2022-07-14 21:50:58 +0900

    업데이트 - 스키마 image 추가
    변경사항 :

--- server/schemas/boards.js
+++ server/schemas/boards.js
@@ -3,11 +3,12 @@ const { Schema } = mongoose;
 
 const boardSchema = new Schema({
   boardId: { type: Number, required: true }, // 포스트 유니크 값
-  workSpaceName: {type: String, required: true},
+  workSpaceName: { type: String, required: true },
   userName: { type: String, required: true }, // 유저 이름
   title: { type: String, required: true }, // 글 제목
   content: { type: String, required: true }, //글 내용
   createdTime: { type: String, required: true },
+  image: { type: String },
   //   likes: { type: Number, default: 0 }, // 좋아요 수
 });
 
