60d232fa939254adf97bfbe8d6788b679ad2402b
hayeonkimm <hayeon.data@gmail.com>
2022-07-14 21:50:18 +0900

    업데이트 - multer 관련작업
    변경사항 :

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -1,11 +1,36 @@
+const dotenv = require('dotenv').config();
 const express = require('express');
 const router = express.Router();
 const postController = require('../controller/posts');
 const authMiddleware = require('../middlewares/auth-middleware');
 const isMember = require('../middlewares/isMember');
+const AWS = require('aws-sdk');
+const multerS3 = require('multer-s3');
+const multer = require('multer');
+const fs = require('fs');
+const path = require('path');
+
+const s3 = new AWS.S3({
+  accessKeyId: process.env.accessKeyId,
+  secretAccessKey: process.env.secretAccessKey,
+  region: 'ap-northeast-2',
+});
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'kimha',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read-write',
+    key: function (req, file, cb) {
+      cb(null, `uploads/${Date.now()}_${file.originalname}`);
+    },
+  }),
+});
+
 //글 작성
 router.post(
   '/post/:workSpaceName',
+  upload.single('img'),
   authMiddleware,
   isMember,
   postController.postUpload
@@ -42,5 +67,7 @@ router.delete(
   isMember,
   postController.postDelete
 );
+
+router.post('/image', upload.single('image'), postController.postImage);
 
 module.exports = router;
