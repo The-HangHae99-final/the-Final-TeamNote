7fc9b9aaddd9592c73566f2746a1cfb67dfea935
hayeonkimm <hayeon.data@gmail.com>
2022-07-15 14:46:54 +0900

    업데이트 - task 주석추가

--- server/routes/likes.js
+++ server/routes/likes.js
@@ -1,12 +1,22 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const likeController = require("../controller/likes")
-const authMiddleware = require("../middlewares/auth-middleware");
-const isMember = require("../middlewares/isMember");
+const likeController = require('../controller/likes');
+const authMiddleware = require('../middlewares/auth-middleware');
+const isMember = require('../middlewares/isMember');
 
 //좋아요
-router.put('/board/post/like/:workSpaceName/:postId', authMiddleware, isMember,likeController.like);
+router.put(
+  '/board/post/like/workSpaceName/:postId',
+  authMiddleware,
+  isMember,
+  likeController.like
+);
 //좋아요 취소
-router.delete('/board/post/like/:workSpaceName/:postId', authMiddleware, isMember,likeController.unlike);
+router.delete(
+  '/board/post/like/workSpaceName/:postId',
+  authMiddleware,
+  isMember,
+  likeController.unlike
+);
 
 module.exports = router;
