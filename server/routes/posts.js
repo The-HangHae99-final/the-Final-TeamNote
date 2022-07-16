d4feaa3cc236793ff23d856d47071195500ab519
hayeonkimm <hayeon.data@gmail.com>
2022-07-16 23:37:46 +0900

    업데이트 - server/routes/posts.js
    변경사항 : 모듈화

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,28 +4,8 @@ const router = express.Router();
 const postController = require('../controller/posts');
 const authMiddleware = require('../middlewares/auth-middleware');
 const isMember = require('../middlewares/isMember');
-const AWS = require('aws-sdk');
-const multerS3 = require('multer-s3');
-const multer = require('multer');
-const fs = require('fs');
-const path = require('path');
-
-const s3 = new AWS.S3({
-  accessKeyId: process.env.accessKeyId,
-  secretAccessKey: process.env.secretAccessKey,
-  region: 'ap-northeast-2',
-});
-const upload = multer({
-  storage: multerS3({
-    s3: s3,
-    bucket: 'kimha',
-    contentType: multerS3.AUTO_CONTENT_TYPE,
-    acl: 'public-read-write',
-    key: function (req, file, cb) {
-      cb(null, `uploads/${Date.now()}_${file.originalname}`);
-    },
-  }),
-});
+const { s3 } = require('../controller/util/aws-s3');
+const { upload } = require('../controller/util/aws-s3');
 
 //일반 게시글
 //글 작성
@@ -39,7 +19,6 @@ router.post(
 );
 
 // 글 전체 조회(임시)
-
 router.post(
   '/post/workSpaceName/all',
   authMiddleware,
@@ -48,20 +27,10 @@ router.post(
 );
 
 // 글 상세 조회
-router.get(
-  '/post/:postId',
-  authMiddleware,
-  isMember,
-  postController.postView
-);
+router.get('/post/:postId', authMiddleware, isMember, postController.postView);
 
 // 글 수정
-router.put(
-  '/post/:postId',
-  authMiddleware,
-  isMember,
-  postController.postEdit
-);
+router.put('/post/:postId', authMiddleware, isMember, postController.postEdit);
 
 // 글 삭제
 router.delete(
@@ -71,6 +40,7 @@ router.delete(
   postController.postDelete
 );
 
+// 이미지 단일 업로드 router, 보류로 주석처리.
 // router.post('/image', upload.single('image'), postController.postImage);
 
 module.exports = router;
