a4092f9203752a75c8de95d8d788b13cbe992225
hayeonkimm <hayeon.data@gmail.com>
2022-07-18 13:12:23 +0900

    업데이트 - 백엔드

--- server/middlewares/isMember.js
+++ server/middlewares/isMember.js
@@ -1,14 +1,14 @@
-// //클라이언트에서 API콜을 받았을때 현재 접속해있는 유저가 해당 워크스페이스의 멤버인지 아닌지를 판별하는 middleware 함수입니다.
-// const workSpace = require('../schemas/workSpace');
+//클라이언트에서 API콜을 받았을때 현재 접속해있는 유저가 해당 워크스페이스의 멤버인지 아닌지를 판별하는 middleware 함수입니다.
+const workSpace = require('../schemas/workSpace');
 
-// module.exports = async (req, res, next) => {
-//   try {
-//     const { workSpaceName } = req.body;
-//     console.log('workSpaceName: ', workSpaceName);
-//     const userEmail = res.locals.User.userEmail;
-//     console.log('userEmail: ', userEmail);
-//     const existWorkSpace = await workSpace.findOne({ name: workSpaceName });
-//     console.log('existWorkSpace: ', existWorkSpace);
+module.exports = async (req, res, next) => {
+  try {
+    const { workSpaceName } = req.body;
+    console.log('workSpaceName: ', workSpaceName);
+    const userEmail = res.locals.User.userEmail;
+    console.log('userEmail: ', userEmail);
+    const existWorkSpace = await workSpace.findOne({ name: workSpaceName });
+    console.log('existWorkSpace: ', existWorkSpace);
 
     if (existWorkSpace === null) {
       res.status(400).send({
