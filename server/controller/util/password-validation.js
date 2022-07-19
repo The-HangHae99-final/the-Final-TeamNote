eb2b0269ab7e4ab4b997c84b82818143b9aa7c83
hayeonkimm <hayeon.data@gmail.com>
2022-07-19 10:18:28 +0900

    업데이트 - password-validation.js 수정

--- server/controller/util/password-validation.js
+++ server/controller/util/password-validation.js
@@ -1,3 +1,5 @@
-export function validatePassword(password, passwordConfirm) {
+function validatePassword(password, passwordConfirm) {
   return password === passwordConfirm; //일치하면 true, 일치하지 않으면 false를 반환한다.
 }
+
+module.exports = { validattePassword };
