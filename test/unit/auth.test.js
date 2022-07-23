6ca1460499d90ba71ee3538f8f85cc6654773df8
hayeonkimm <hayeon.data@gmail.com>
2022-07-23 13:49:06 +0900

    update auth.test.js

--- test/unit/auth.test.js
+++ test/unit/auth.test.js
@@ -20,10 +20,35 @@ test('비밀번호를 입력했을때 password와 passwordcheck가 일치하지 
   expect(validatePassword('aaAA11!@', 'aaaaabbb')).toEqual(false);
 });
 
-describe('user Controller signup test', () => {
-  it('should have a user function', () => {
+describe('user Controller signup, emailFirst 테스트', () => {
+  it('should have a user signup function', () => {
     // userController에 createProduct가 함수인지 파악하는것
     // 예상을 하면서 만드는것
     expect(typeof userController.signup).toBe('function');
+  });
+  it('should have a user emailFirst function', () => {
+    // userController에 createProduct가 함수인지 파악하는것
+    // 예상을 하면서 만드는것
+    expect(typeof userController.emailFirst).toBe('function');
+  });
+  it('should have a user passwordSecond function', () => {
+    // userController에 createProduct가 함수인지 파악하는것
+    // 예상을 하면서 만드는것
+    expect(typeof userController.passwordSecond).toBe('function');
+  });
+  it('should have a user deleteUser function', () => {
+    // userController에 createProduct가 함수인지 파악하는것
+    // 예상을 하면서 만드는것
+    expect(typeof userController.deleteUser).toBe('function');
+  });
+  it('should have a user searchUser function', () => {
+    // userController에 createProduct가 함수인지 파악하는것
+    // 예상을 하면서 만드는것
+    expect(typeof userController.searchUser).toBe('function');
+  });
+  it('should have a user mailing function', () => {
+    // userController에 createProduct가 함수인지 파악하는것
+    // 예상을 하면서 만드는것
+    expect(typeof userController.mailing).toBe('function');
   });
 });
