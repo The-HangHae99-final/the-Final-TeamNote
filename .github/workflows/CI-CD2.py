7e461942972dc4cf736321713154595996124495
hayeonkimm <hayeon.data@gmail.com>
2022-08-15 06:44:41 +0900

    업데이트 - CI-CD.yaml rechange

---
+++ .github/workflows/CI-CD2.py
@@ -0,0 +1,26 @@
+# name: CI-CD2
+# on:
+#   push:
+#     branches:
+#       - main
+#   pull_request:
+#     branches:
+#       - main
+# jobs:
+#   build:
+#     runs-on: ubuntu-latest
+#     steps:
+#       - name: Deploy
+#         uses: appleboy/ssh-action@master
+#         with:
+#           host: ${{ secrets.HOST }}
+#           username: ubuntu
+#           key: ${{ secrets.PRIVATE_KEY }}
+#           port: 22
+#           script: |
+#             cd /home/ubuntu/the-Final-TeamNote
+#             git config --global --add safe.directory /home/ubuntu/the-Final-TeamNote
+#             sudo git pull
+#             sudo npm update
+#             sudo npm install
+#             sudo pm2 reload all
